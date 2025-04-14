de29c68b60e3b4cbf9cbfa613f973195bf8f907a
zzyyppqq <zzyyppqq@gmail.com>
2025-04-14 22:56:17 +0800

    update

--- User/Src/Delay.c
+++ User/Src/Delay.c
@@ -2,12 +2,12 @@
 
 /**
   * @brief  微秒级延时
-  * @param  xus 延时时长，范围：0~233015
+  * @param  us 延时时长，范围：0~233015
   * @retval 无
   */
-void delay_us(uint32_t xus)
+void Delay_us(uint32_t us)
 {
-	SysTick->LOAD = 72 * xus;				//设置定时器重装值
+	SysTick->LOAD = 72 * us;				//设置定时器重装值
 	SysTick->VAL = 0x00;					//清空当前计数值
 	SysTick->CTRL = 0x00000005;				//设置时钟源为HCLK，启动定时器
 	while(!(SysTick->CTRL & 0x00010000));	//等待计数到0
@@ -16,26 +16,26 @@ void delay_us(uint32_t xus)
 
 /**
   * @brief  毫秒级延时
-  * @param  xms 延时时长，范围：0~4294967295
+  * @param  ms 延时时长，范围：0~4294967295
   * @retval 无
   */
-void delay_ms(uint32_t xms)
+void Delay_ms(uint32_t ms)
 {
-	while(xms--)
+	while(ms--)
 	{
-		delay_us(1000);
+        Delay_us(1000);
 	}
 }
  
 /**
   * @brief  秒级延时
-  * @param  xs 延时时长，范围：0~4294967295
+  * @param  s 延时时长，范围：0~4294967295
   * @retval 无
   */
-void delay_s(uint32_t xs)
+void Delay_s(uint32_t s)
 {
-	while(xs--)
+	while(s--)
 	{
-		delay_ms(1000);
+        Delay_ms(1000);
 	}
 } 
